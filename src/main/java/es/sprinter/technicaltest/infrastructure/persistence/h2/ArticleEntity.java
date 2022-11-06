023c7cdb71f6a425b761b10b5289f6be7a4380a5
Daniel Mato Berciano <daniel.mato.b@gmail.com>
2022-11-06 22:13:45 +0100

    Se modifica la forma en la que se genera el ID del articulo.
    Por defecto estaba consultando una secuencia cuando en el propio script
    de creación de la tabla se indica que es un campo auto incremental. Se
    modifica para reflejarlo en la entidad JPA.

--- src/main/java/es/sprinter/technicaltest/infrastructure/persistence/h2/ArticleEntity.java
+++ src/main/java/es/sprinter/technicaltest/infrastructure/persistence/h2/ArticleEntity.java
@@ -16,7 +16,7 @@ public class ArticleEntity {
 
 	@Id
 	@Column(name = "ID")
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	@Column(name = "CODIGO", unique = true)
