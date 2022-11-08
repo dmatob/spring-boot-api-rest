c6759dc715b214cc1395a7279a023806ccfa2c81
Daniel Mato Berciano <daniel.mato.b@gmail.com>
2022-11-08 20:35:50 +0100

    Documentación de la API con springdoc-openapi

--- src/main/java/es/sprinter/technicaltest/application/rest/ArticleController.java
+++ src/main/java/es/sprinter/technicaltest/application/rest/ArticleController.java
@@ -23,55 +23,108 @@ import es.sprinter.technicaltest.application.rest.dto.ArticleDTO;
 import es.sprinter.technicaltest.application.rest.dto.ArticleModificationRequestDTO;
 import es.sprinter.technicaltest.application.rest.dto.ArticlePriceModificationRequestDTO;
 import es.sprinter.technicaltest.domain.service.ArticleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
 @RequestMapping("/articles")
 public class ArticleController {
 
 	private static final Logger LOG = LoggerFactory.getLogger(ArticleController.class);
-	
+
 	private final ArticleService articleService;
-		
+
 	public ArticleController(ArticleService articleService) {
 		this.articleService = articleService;
 	}
+
 	
+	@Operation(summary = "Crear un nuevo artículo")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Artículo creado", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ArticleDTO.class)) }),
+			@ApiResponse(responseCode = "400", description = "Petición incorrecta", content = @Content) })
 	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	ResponseEntity<ArticleDTO> createArticle(@RequestBody @Valid final ArticleModificationRequestDTO articleDataDTO) {
 		LOG.info("Llamada a la API de articulos para crear un nuevo articulo");
-		return new ResponseEntity<>(ArticleDTOMapper.toArticleDTO(this.articleService.createArticle(ArticleDTOMapper.fromArticleDTO(articleDataDTO))), HttpStatus.CREATED);
+		return new ResponseEntity<>(
+				ArticleDTOMapper.toArticleDTO(
+						this.articleService.createArticle(ArticleDTOMapper.fromArticleDTO(articleDataDTO))),
+				HttpStatus.CREATED);
 	}
+
 	
+	@Operation(summary = "Obtener el listado de todos los articulos existentes")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Consulta realizada con éxito", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ArticleDTO.class)) }),
+			@ApiResponse(responseCode = "400", description = "Petición incorrecta", content = @Content) })
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	ResponseEntity<List<ArticleDTO>> getAllArticles() {
 		LOG.info("Llamada a la API de articulos para obtener el listado de todos los articulos disponibles");
 		return ResponseEntity.ok(ArticleDTOMapper.toListOfArticleDTOs(this.articleService.getAllArticles()));
 	}
 	
+
+	@Operation(summary = "Obtener información de un artículo en base a su código")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Consulta realizada con éxito", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ArticleDTO.class)) }),
+			@ApiResponse(responseCode = "400", description = "Petición incorrecta", content = @Content) })
 	@GetMapping(value = "/{articleCode}", produces = MediaType.APPLICATION_JSON_VALUE)
 	ResponseEntity<ArticleDTO> getArticleByCode(@PathVariable final String articleCode) {
 		LOG.info("Llamada a la API de articulos para obtener informacion del articulo con codigo {}", articleCode);
 		return ResponseEntity.ok(ArticleDTOMapper.toArticleDTO(this.articleService.getArticle(articleCode)));
 	}
 	
+
+	@Operation(summary = "Actualizar un artículo existente")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Actualización realizada con éxito", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ArticleDTO.class)) }),
+			@ApiResponse(responseCode = "400", description = "Petición incorrecta", content = @Content),
+			@ApiResponse(responseCode = "404", description = "Artículo no encontrado", content = @Content)})
 	@PutMapping(value = "/{articleCode}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-	ResponseEntity<ArticleDTO> updateArticle(@PathVariable final String articleCode, @RequestBody @Valid final ArticleModificationRequestDTO articleDataDTO) {
-		LOG.info("Llamada a la API de articulos para modificar la informacion del articulo con codigo: {}", articleCode);
-		return new ResponseEntity<>(ArticleDTOMapper.toArticleDTO(this.articleService.updateArticle(articleCode, ArticleDTOMapper.fromArticleDTO(articleDataDTO))), HttpStatus.OK);
+	ResponseEntity<ArticleDTO> updateArticle(@PathVariable final String articleCode,
+			@RequestBody @Valid final ArticleModificationRequestDTO articleDataDTO) {
+		LOG.info("Llamada a la API de articulos para modificar la informacion del articulo con codigo: {}",
+				articleCode);
+		return new ResponseEntity<>(ArticleDTOMapper.toArticleDTO(
+				this.articleService.updateArticle(articleCode, ArticleDTOMapper.fromArticleDTO(articleDataDTO))),
+				HttpStatus.OK);
 	}
 	
+
+	@Operation(summary = "Actualizar el precio de un artículo existente")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Actualización de precio realizada con éxito", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ArticleDTO.class)) }),
+			@ApiResponse(responseCode = "400", description = "Petición incorrecta", content = @Content),
+			@ApiResponse(responseCode = "404", description = "Artículo no encontrado", content = @Content)})
 	@PatchMapping(value = "/{articleCode}/price", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-	ResponseEntity<ArticleDTO> updatePriceArticle(@PathVariable final String articleCode, @RequestBody @Valid final ArticlePriceModificationRequestDTO articleDataDTO) {
+	ResponseEntity<ArticleDTO> updatePriceArticle(@PathVariable final String articleCode,
+			@RequestBody @Valid final ArticlePriceModificationRequestDTO articleDataDTO) {
 		LOG.info("Llamada a la API de articulos para modificar el precio del articulo con codigo: {}", articleCode);
-		return new ResponseEntity<>(ArticleDTOMapper.toArticleDTO(this.articleService.updatePriceArticleByCode(articleCode, articleDataDTO.getPrice())), HttpStatus.OK);
+		return new ResponseEntity<>(
+				ArticleDTOMapper.toArticleDTO(
+						this.articleService.updatePriceArticleByCode(articleCode, articleDataDTO.getPrice())),
+				HttpStatus.OK);
 	}
+
 	
+	@Operation(summary = "Eliminar un artículo existente")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Borrado realizado con éxito", content = @Content),
+			@ApiResponse(responseCode = "400", description = "Petición incorrecta", content = @Content),
+			@ApiResponse(responseCode = "404", description = "Artículo no encontrado", content = @Content)})
 	@DeleteMapping(value = "/{articleCode}")
 	ResponseEntity<Void> deleteArticle(@PathVariable final String articleCode) {
 		LOG.info("Llamada a la API de articulos para eliminar el articulo con codigo: {}", articleCode);
 		this.articleService.deleteArticleByCode(articleCode);
 		return ResponseEntity.ok().build();
 	}
-
 
 }
--- src/main/java/es/sprinter/technicaltest/application/rest/RestExceptionHandler.java
+++ src/main/java/es/sprinter/technicaltest/application/rest/RestExceptionHandler.java
@@ -10,6 +10,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -21,24 +22,28 @@ import es.sprinter.technicaltest.domain.exception.InvalidArticlePriceException;
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(value = { ArticleNotFoundException.class })
+	@ResponseStatus(HttpStatus.NOT_FOUND)
 	protected ResponseEntity<Object> articleNotFound(RuntimeException ex, WebRequest request) {
 		String bodyOfResponse = "El artículo especificado no existe";
 		return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
 	}
 
 	@ExceptionHandler(value = { InvalidArticlePriceException.class })
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	protected ResponseEntity<Object> invalidArticlePrice(RuntimeException ex, WebRequest request) {
 		String bodyOfResponse = "El precio del artículo es incorrecto. El precio ha de ser mayor a 0€";
 		return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
 	}
 
 	@ExceptionHandler(value = { DuplicatedArticleException.class })
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	protected ResponseEntity<Object> duplicatedArtcile(RuntimeException ex, WebRequest request) {
 		String bodyOfResponse = "El articulo ya existe";
 		return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
 	}
 
 	@Override
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
 
