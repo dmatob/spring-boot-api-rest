c1090363128b87e9ec8119c52ed0fa0142c9bb49
Daniel Mato <daniel.mato.b@gmail.com>
2023-01-24 12:27:10 +0100

    Organización de imports

--- src/test/java/es/sprinter/technicaltest/ArticlesApplicationTests.java
+++ src/test/java/es/sprinter/technicaltest/ArticlesApplicationTests.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
