81f3a4cb8522b0aab2599b6fc8ec3c53a2d67ae1
Daniel Mato Berciano <daniel.mato.b@gmail.com>
2023-09-26 11:08:41 +0200

    Ajuste base de datos Cambios para incluir a nivel de BBDD el concepto categoria de artíclos.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,19 @@
-INSERT INTO ARTICULO_TIPO (CODIGO, DESCRIPCION) VALUES
-  ('ZAPATILLA', 'Zapatillas deportivas'),
-  ('MALLA', 'Mallas deportivas'),
-  ('CHAQUETA', 'Chaquetas deportivas'),
-  ('BOTAS', 'Botas de montaña'),
-  ('BANADOR', 'Bañador');
+INSERT INTO ARTICULO_CATEGORIA (CODIGO, DESCRIPCION) VALUES
+  ('CALZADO', 'Calzado'),
+  ('ROPA', 'Moda deportiva'),
+  ('BANO', 'Articulos de baño / natación');
+
+INSERT INTO ARTICULO_TIPO (CODIGO, ID_ARTICULO_CATEGORIA, DESCRIPCION) VALUES
+  ('ZAPATILLA', (SELECT ID FROM ARTICULO_CATEGORIA WHERE CODIGO = 'CALZADO'), 'Zapatillas deportivas'),
+  ('MALLA', (SELECT ID FROM ARTICULO_CATEGORIA WHERE CODIGO = 'ROPA'), 'Mallas deportivas'),
+  ('CHAQUETA', (SELECT ID FROM ARTICULO_CATEGORIA WHERE CODIGO = 'ROPA'), 'Chaquetas deportivas'),
+  ('BOTAS', (SELECT ID FROM ARTICULO_CATEGORIA WHERE CODIGO = 'CALZADO'), 'Botas de montaña'),
+  ('BANADOR', (SELECT ID FROM ARTICULO_CATEGORIA WHERE CODIGO = 'BANO'), 'Bañador');
 
 INSERT INTO ARTICULO (CODIGO, ID_ARTICULO_TIPO, DESCRIPCION, PRECIO_EUROS, ULTIMA_MODIFICACION) VALUES
-  ('converse-boulevard', (SELECT ID FROM ARTICULO_TIPO WHERE CODIGO = 'ZAPATILLA'), 'Zapatillas deportivas Converse Boulevard para hombre', 49.99, NULL),
-  ('asics-icon', (SELECT ID FROM ARTICULO_TIPO WHERE CODIGO = 'MALLA'), 'Mallas de running Ascis Icon para mujer', 54.99, NULL),
-  ('the-north-face-quest', (SELECT ID FROM ARTICULO_TIPO WHERE CODIGO = 'CHAQUETA'), 'Chaqueta de hombre para deportes de montaña de la marca The North Face', 149.99, '2022-11-06 23.03.10'),
-  ('adidas-terrex-ax3', (SELECT ID FROM ARTICULO_TIPO WHERE CODIGO = 'BOTAS'), 'Botas de montaña para hombre Adidas Terrex AX3', 159.99, NULL),
-  ('puma-racerback', (SELECT ID FROM ARTICULO_TIPO WHERE CODIGO = 'BANADOR'), 'Bañador de la marca Puma para mujer', 34.99, NULL),
-  ('botas-chiruca-montreal', (SELECT ID FROM ARTICULO_TIPO WHERE CODIGO = 'BOTAS'), 'Botas de montaña Chiruca para hombre', 119.99, '2022-10-21 11.24.11');
+  ('CONVERSE-BOULEVARD', (SELECT ID FROM ARTICULO_TIPO WHERE CODIGO = 'ZAPATILLA'), 'Zapatillas deportivas Converse Boulevard para hombre', 49.99, NULL),
+  ('ASICS-ICON', (SELECT ID FROM ARTICULO_TIPO WHERE CODIGO = 'MALLA'), 'Mallas de running Ascis Icon para mujer', 54.99, NULL),
+  ('THE-NORTH-FACE-QUEST', (SELECT ID FROM ARTICULO_TIPO WHERE CODIGO = 'CHAQUETA'), 'Chaqueta de hombre para deportes de montaña de la marca The North Face', 149.99, '2022-11-06 23.03.10'),
+  ('ADIDAS-TERREX-AX3', (SELECT ID FROM ARTICULO_TIPO WHERE CODIGO = 'BOTAS'), 'Botas de montaña para hombre Adidas Terrex AX3', 159.99, NULL),
+  ('PUMA-RACERBACK', (SELECT ID FROM ARTICULO_TIPO WHERE CODIGO = 'BANADOR'), 'Bañador de la marca Puma para mujer', 34.99, NULL),
+  ('BOTAS-CHIRUCA-MONTREAL', (SELECT ID FROM ARTICULO_TIPO WHERE CODIGO = 'BOTAS'), 'Botas de montaña Chiruca para hombre', 119.99, '2022-10-21 11.24.11');
